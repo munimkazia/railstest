75c4245363cc22ce09d08b2cf8d49acb5e41a606
Abdul Munim Kazia <munim2020@gmail.com>
2015-01-05 15:58:05 +0530

    auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "munim", password: "munim123", except: [:index, :show]
+
+
   def index
     @articles = Article.all
   end
